b8460e4261f336f675c23fc81f366f8d38a70c3a
Nithyadevi <nithi2218@gmail.com>
2022-04-01 23:14:45 +1100

    Assignment Completed

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+ static capitalize(str) {
+   return str.charAt(0).toUpperCase()+str.slice(1)
+ }
+
+ static sanitize(str) {
+   return str.replace(/[^-' A-Za-z0-9]+/g,'');
+ }
+ 
+ static titleize(str)  {
+ const words = []
+ str.split(" ").forEach((word, index) => {
+   if (index === 0 || !['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'].includes(word)) {
+   words.push(this.capitalize(word))     
+   }
+   else {
+     words.push(word)
+   }
+   
+ })
+return words.join(" ")
+ }
+
 }
